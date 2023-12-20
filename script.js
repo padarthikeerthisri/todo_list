e7c9b251bdd3e7f32af5bb0f85748fe4d983a3f1
padarthikeerthisri <154013810+padarthikeerthisri@users.noreply.github.com>
2023-12-20 12:25:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        const img = document.createElement("img"); 
+        img.src= "images/deleteicon.png";
+        li.appendChild(img);
+    }
+    inputBox.value="";
+    saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    }
+    saveData();
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
